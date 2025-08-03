115dd555ea0a6eee950fd7b22643962639e51e1d
ZyanAlkanza <zyan.mujaddid@gmail.com>
2025-08-03 14:25:40 +0700

    fix: params di users/id/route.ts

--- src/app/api/users/id/route.ts
+++ src/app/api/users/id/route.ts
@@ -1,23 +1,23 @@
 import { prisma } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
-export async function GET(_: Request, { params }: { params: { id: number } }) {
-  const user = await prisma.users.findUnique({ where: { id: params.id } });
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+  const user = await prisma.users.findUnique({ where: { id: parseInt(params.id) } });
   return user
     ? NextResponse.json(user)
     : NextResponse.json({ error: 'User not found' }, { status: 404 });
 }
 
-export async function PUT(req: Request, { params }: { params: { id: number } }) {
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
   const data = await req.json();
   const user = await prisma.users.update({
-    where: { id: params.id },
+    where: { id: parseInt(params.id) },
     data,
   });
   return NextResponse.json(user);
 }
 
-export async function DELETE(_: Request, { params }: { params: { id: number } }) {
-  await prisma.users.delete({ where: { id: params.id } });
+export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+  await prisma.users.delete({ where: { id: parseInt(params.id) } });
   return NextResponse.json({ message: 'Deleted' });
 }
